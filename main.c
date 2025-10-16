2da98a88e208958d94a069cea329887cd2c17253
Pasiya007 <rapmanoj2004@gmail.com>
2025-10-16 16:35:03 +0530

    create storeDistances and addDistances function; Update(2) readCities function(get cities to an array at the end) and addCities function

--- main.c
+++ main.c
@@ -4,10 +4,15 @@
 
 #define MAX_CITIES 30
 
-void storeCities(char cityName[MAX_CITIES][20]);
-void addCities(char cityName[MAX_CITIES][20]);
+void storeCities(char cityName[MAX_CITIES][20]); //this have to remove
+void addCities(char cityName[MAX_CITIES][20],int distance[MAX_CITIES][MAX_CITIES]);
 int countCities();
 void readCities(char cityName[MAX_CITIES][20]);
+
+void storeDistances(int distance[MAX_CITIES][MAX_CITIES]);
+void addDistances(int distance[MAX_CITIES][MAX_CITIES],char cityName[MAX_CITIES][20]);
+void readDistances(int distance[MAX_CITIES][20]);
+
 
 
 
@@ -21,20 +26,33 @@ int main()
     strcpy(cityName[2], "Galle");
     strcpy(cityName[3], "Jaffna");
     strcpy(cityName[4], "Anuradhapura");
-    strcpy(cityName[5], "Polonnaruwa");
 
-    //storeCities(cityName);
+
+    int distance[30][30] =
+    {
+        {0, 100, 200, 300, 400},
+        {100, 0, 150, 250, 350},
+        {200, 150, 0, 120, 220},
+        {300, 250, 120, 0, 110},
+        {400, 350, 220, 110, 0}
+    };
+
+    storeCities(cityName);//this have to remove
+    storeDistances(distance);//this have to remove
+
     readCities(cityName);
-    addCities(cityName);
+    addCities(cityName,distance);
     readCities(cityName);
+
+    storeDistances(distance);
 
     return 0;
 }
 
-void storeCities(char cityName[MAX_CITIES][20])
+void storeCities(char cityName[MAX_CITIES][20]) //this have to remove
 {
     int i;
-    int num_cities=6;
+    int num_cities=5;
 
     FILE *cities_file = fopen("cities.txt", "w");
 
@@ -57,7 +75,7 @@ void storeCities(char cityName[MAX_CITIES][20])
     }
 }
 
-void addCities(char cityName[MAX_CITIES][20])
+void addCities(char cityName[MAX_CITIES][20],int distance[MAX_CITIES][MAX_CITIES])
 {
     int num_cities=countCities();
     char newCity[20];
@@ -80,6 +98,9 @@ void addCities(char cityName[MAX_CITIES][20])
         fclose(cities_file);
         printf("\t***City list (total %d) successfully Added to cities.txt.\n", num_cities+1);
     }
+
+    //addDistances
+    addDistances(distance,cityName);
 }
 
 int countCities()
@@ -92,15 +113,16 @@ int countCities()
         return 0;
     }
 
- else
+    else
     {
         int read = '\0';
         int count=0;
         while ((read = fgetc(cities_file))!= EOF)
         {
-            if (read == '\n') {
-            count++;
-        }
+            if (read == '\n')
+            {
+                count++;
+            }
         }
 
         fclose(cities_file);
@@ -111,28 +133,104 @@ int countCities()
 
 void readCities(char cityName[MAX_CITIES][20])
 {
-    int num_cities=countCities();
+    int num_cities = countCities();
+    if (num_cities == 0)
+    {
+        printf("\t***City list is empty.\n");
+        return;
+    }
 
     FILE *cities_file = fopen("cities.txt", "r");
 
     if (cities_file == NULL)
     {
-        printf("\n\t!!!Error: Could not open cities.txt for writing.\n");
+        printf("\n\t!!!Error: Could not open cities.txt for reading.\n");
+        return;
+    }
+
+    for (int i = 0; i < num_cities; i++)
+    {
+        fgets(cityName[i], MAX_CITIES, cities_file);
+
+        printf("%d. %s", i + 1, cityName[i]);//new line was already added due to fgets function
+    }
+
+    printf("\t***City list (total %d) successfully read from cities.txt.\n", num_cities);
+    fclose(cities_file);
+}
+
+void storeDistances(int distance[MAX_CITIES][MAX_CITIES])
+{
+    int num_cities=countCities();
+
+    FILE *distances_file = fopen("distances.txt", "w");
+
+    if (distances_file == NULL)
+    {
+        printf("\t!!!Error: Could not open distances.txt for writing.\n");
         return;
     }
 
     else
     {
-        printf("\n");
 
-        int read = '\0';
-        while ((read = fgetc(cities_file))!= EOF)
+        for (int i = 0; i < num_cities; i++)
         {
-            printf("%c",read);
+            for (int j = 0; j < num_cities; j++)
+            {
+
+                fprintf(distances_file, "%d ", distance[i][j]);
+            }
+
+            fprintf(distances_file, "\n");
         }
 
-        printf("\t***City list (total %d) successfully read from cities.txt.\n", num_cities);
-        fclose(cities_file);
+        fclose(distances_file);
+        // printf("\t***City list (total %d) successfully stored to cities.txt.\n", num_cities);
     }
+
 }
 
+void addDistances(int distance[MAX_CITIES][MAX_CITIES],char cityName[MAX_CITIES][20])
+{
+    int num_cities=countCities()-1; // Since we add a new city name, countCities gives the new count. in addDistance we need previous count.
+
+    FILE *distances_file = fopen("distances.txt", "r");
+
+    if (distances_file == NULL)
+    {
+        printf("\t!!!Error: Could not open distances.txt for Adding.\n");
+        return;
+    }
+
+    else
+    {
+
+
+        for(int i=0; i<num_cities; i++)
+        {
+            for(int j=0; j<num_cities; j++)
+            {
+                (fscanf(distances_file, "%d", &distance[i][j]));
+            }
+        }
+
+        int tempDistances[num_cities]; //
+
+        for(int i=0; i<num_cities && cityName[i] != NULL; i++)
+        {
+
+            printf("\nEnter distance to %s :",&cityName[i]);
+            scanf("%d",&tempDistances[i]);
+
+            distance[num_cities][i]=tempDistances[i];
+            distance[i][num_cities]=tempDistances[i];//to create symmetrical matrix
+        }
+        distance[num_cities][num_cities]=0; //same city
+
+        storeDistances(distance);
+
+        fclose(distances_file);
+        // printf("\t***City list (total %d) successfully stored to cities.txt.\n", num_cities);
+    }
+}
