3c04c45a493621efa30b25b664f1b66d0f77f3c6
Pasiya007 <rapmanoj2004@gmail.com>
2025-10-16 07:39:50 +0530

    create: countCities function and update(1): readCities function and addCities function

--- main.c
+++ main.c
@@ -4,9 +4,10 @@
 
 #define MAX_CITIES 30
 
-void storeCities(char cityName[MAX_CITIES][20], int num_cities);
-int addCities(char cityName[MAX_CITIES][20], int num_cities);
-void getCities(char cityName[MAX_CITIES][20], int num_cities);
+void storeCities(char cityName[MAX_CITIES][20]);
+void addCities(char cityName[MAX_CITIES][20]);
+int countCities();
+void readCities(char cityName[MAX_CITIES][20]);
 
 
 
@@ -14,7 +15,6 @@ void getCities(char cityName[MAX_CITIES][20], int num_cities);
 int main()
 {
     char cityName[MAX_CITIES][20];
-    int num_cities=6;
 
     strcpy(cityName[0], "Colombo");
     strcpy(cityName[1], "Kandy");
@@ -23,23 +23,24 @@ int main()
     strcpy(cityName[4], "Anuradhapura");
     strcpy(cityName[5], "Polonnaruwa");
 
-    storeCities(cityName,num_cities);
-    getCities(cityName,num_cities);
-    num_cities= addCities(cityName,num_cities);
-    getCities(cityName,num_cities);
+    //storeCities(cityName);
+    readCities(cityName);
+    addCities(cityName);
+    readCities(cityName);
 
     return 0;
 }
 
-void storeCities(char cityName[MAX_CITIES][20], int num_cities)
+void storeCities(char cityName[MAX_CITIES][20])
 {
     int i;
+    int num_cities=6;
 
     FILE *cities_file = fopen("cities.txt", "w");
 
     if (cities_file == NULL)
     {
-        printf("Error: Could not open cities.txt for writing.\n");
+        printf("\t!!!Error: Could not open cities.txt for writing.\n");
         return;
     }
 
@@ -52,12 +53,13 @@ void storeCities(char cityName[MAX_CITIES][20], int num_cities)
         }
 
         fclose(cities_file);
-        printf("City list (total %d) successfully stored to cities.txt.\n", num_cities);
+        printf("\t***City list (total %d) successfully stored to cities.txt.\n", num_cities);
     }
 }
 
-int addCities(char cityName[MAX_CITIES][20], int num_cities)
+void addCities(char cityName[MAX_CITIES][20])
 {
+    int num_cities=countCities();
     char newCity[20];
 
     FILE *cities_file = fopen("cities.txt", "a");
@@ -67,44 +69,69 @@ int addCities(char cityName[MAX_CITIES][20], int num_cities)
 
     if (cities_file == NULL)
     {
-        printf("Error: Could not open cities.txt for append.\n");
-        return 0;
-    }
-
-    else
-    {
-        int i=0;
-
-        fprintf(cities_file, "%s",newCity);
-        num_cities++;
-
-        fclose(cities_file);
-        printf("City list (total %d) successfully Added to cities.txt.\n", num_cities);
-
-        return num_cities;
-    }
-}
-
-void getCities(char cityName[MAX_CITIES][20], int num_cities)
-{
-    FILE *cities_file = fopen("cities.txt", "r");
-
-    if (cities_file == NULL)
-    {
-        printf("Error: Could not open cities.txt for reading.\n");
+        printf("\t!!!Error: Could not open cities.txt for append.\n");
         return;
     }
 
     else
     {
-        for (int i = 0; i <num_cities ; i++)
-        {
+        fprintf(cities_file, "%s",newCity);
 
-            fscanf(cities_file, "%s",cityName[i]);
-            printf("\n %d.\t%s",i+1,cityName[i]);
+        fclose(cities_file);
+        printf("\t***City list (total %d) successfully Added to cities.txt.\n", num_cities+1);
+    }
+}
+
+int countCities()
+{
+    FILE *cities_file = fopen("cities.txt", "r");
+
+    if (cities_file == NULL)
+    {
+        printf("\t!!!Error: Could not open cities.txt for count cities.\n");
+        return 0;
+    }
+
+ else
+    {
+        int read = '\0';
+        int count=0;
+        while ((read = fgetc(cities_file))!= EOF)
+        {
+            if (read == '\n') {
+            count++;
+        }
         }
 
+        fclose(cities_file);
+        return count;
+    }
+}
 
+
+void readCities(char cityName[MAX_CITIES][20])
+{
+    int num_cities=countCities();
+
+    FILE *cities_file = fopen("cities.txt", "r");
+
+    if (cities_file == NULL)
+    {
+        printf("\n\t!!!Error: Could not open cities.txt for writing.\n");
+        return;
+    }
+
+    else
+    {
+        printf("\n");
+
+        int read = '\0';
+        while ((read = fgetc(cities_file))!= EOF)
+        {
+            printf("%c",read);
+        }
+
+        printf("\t***City list (total %d) successfully read from cities.txt.\n", num_cities);
         fclose(cities_file);
     }
 }
